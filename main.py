58c0fcd345f93d56599e172e91c4c879dfdd23eb
Peter Boucher <petebouch@gmail.com>
2020-09-13 14:39:31 +0200

    initial setup

---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+import random
+
+chore_list = {}
+board = []
+
+
+def select_random_chore():
+    selected_chore = random.choice(chore_list)
+    chore_list.remove(selected_chore)
+    return selected_chore
+
+
+def load_chores():
+    with open("chores.json") as chore_file:
+        chores_json = chore_file.read()
+        return json.loads(chores_json)
+
+
+def setup_board(helpers):
+    for helper in helpers:
+        board.append({'name': helper, 'chores': []})
+
+
+def output_board():
+    for row in board:
+        print(row['name'])
+        for item in row['chores']:
+            print(item['name'], end=' | ')
+        print()
+
+
+if __name__ == "__main__":
+    staff = ["Jodie", "Pete", "Maya", "Lucy"]
+    print("Staff", staff)
+    chore_list = load_chores()
+    # print("Chores", chore_list)
+    setup_board(staff)
+    print(board)
+
+    while len(chore_list) > 0:
+        for row in board:
+            row['chores'].append(select_random_chore())
+            print("row", row)
+
+    output_board()
